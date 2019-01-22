c61a66a731265962799adc6b48d234d73a7ce23f
Lukas Motiejūnas <motiejunas.lukas@gmail.com>
2019-01-22 23:12:42 +0200

    all_2

---
+++ service_app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by motie on 2018-12-27.
+ */
+/////////////////////// LIBRARIES ////////////////////
+const commandLineArgs = require('command-line-args')
+const MongoClient = require('mongodb').MongoClient
+const express = require('express');
+const app = express();
+
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
